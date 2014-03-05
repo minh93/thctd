696f7c37d9a4b8c79586b74648fb9b81a7e7ea3b
hedspi <hespi@localhost.localdomain>
2014-03-05 15:12:16 +0700

    scanner done

--- Bai2/incompleted/error.h
+++ Bai2/incompleted/error.h
@@ -6,18 +6,23 @@
 
 #ifndef __ERROR_H__
 #define __ERROR_H__
+#include "token.h"
 
 typedef enum {
   ERR_ENDOFCOMMENT,
   ERR_IDENTTOOLONG,
   ERR_INVALIDCHARCONSTANT,
-  ERR_INVALIDSYMBOL
+  ERR_INVALIDSTRING,
+  ERR_INVALIDNUMBER,
+  ERR_INVALIDSYMBOL,
 } ErrorCode;
 
 
 #define ERM_ENDOFCOMMENT "End of comment expected!"
 #define ERM_IDENTTOOLONG "Identification too long!"
 #define ERM_INVALIDCHARCONSTANT "Invalid const char!"
+#define ERM_INVALIDSTRING "Invalid string!"
+#define ERM_INVALIDNUMBER "Invalid number!"
 #define ERM_INVALIDSYMBOL "Invalid symbol!"
 
 void error(ErrorCode err, int lineNo, int colNo);
