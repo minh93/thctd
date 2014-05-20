6cd89107a9184d550be5369ff4d34dbeb6601a78
Minh PD <phamducminh.bri@gmail.com>
2014-05-21 00:24:28 +0700

    week 5

---
+++ Bai5/charcode.h
@@ -0,0 +1,35 @@
+/* 
+ * @copyright (c) 2008, Hedspi, Hanoi University of Technology
+ * @author Huu-Duc Nguyen
+ * @version 1.0
+ */
+
+#ifndef __CHARCODE_H__
+#define __CHARCODE_H__
+
+typedef enum {
+  CHAR_SPACE,         //    space
+  CHAR_LETTER,        //    letter
+  CHAR_DIGIT,         //    digital
+  CHAR_PLUS,          //    +
+  CHAR_MINUS,         //    -
+  CHAR_TIMES,         //    *
+  CHAR_SLASH,         //    /
+  CHAR_PERCENT,       //    %
+  CHAR_BACKSLASH,     //    
+  CHAR_LT,            //    <
+  CHAR_GT,            //    >
+  CHAR_EXCLAIMATION,  //    !
+  CHAR_EQ,            //    =
+  CHAR_COMMA,         //    ,
+  CHAR_PERIOD,        //    .
+  CHAR_COLON,         //    :
+  CHAR_SEMICOLON,     //    ;
+  CHAR_SINGLEQUOTE,   //    '
+  CHAR_QUOTE,         //    "
+  CHAR_LPAR,          //    (
+  CHAR_RPAR,          //    )
+  CHAR_UNKNOWN
+} CharCode;
+
+#endif
